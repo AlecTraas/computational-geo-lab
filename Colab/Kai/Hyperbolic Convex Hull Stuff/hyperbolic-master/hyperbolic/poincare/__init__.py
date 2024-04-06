7da44a6221100b1156cabaf830a500d44b566c12
Kai Garcia-Lorincz <106930751+KGL8@users.noreply.github.com>
2024-04-06 18:59:12 -0400

    Delete Colab/Kai/Hyperbolic Convex Hull Stuff directory

--- Colab/Kai/Hyperbolic Convex Hull Stuff/hyperbolic-master/hyperbolic/poincare/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import util
-from .point import Point, Ideal
-from .line import Line
-from .hypercycle import Hypercycle
-from .horocycle import Horocycle
-from .circle import Circle
-from .polygon import Polygon
-from .transform import Transform
--- Colab/Kai/Hyperbolic Convex Hull Stuff/info.md
+++
@@ -1,1 +0,0 @@
-To run my code on your own machine, download the `Hyperbolic Convex Hull Stuff` folder, and follow installation instructions in the /hyperbolic-master Readme.
